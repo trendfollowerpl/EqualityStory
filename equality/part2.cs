1725bfe27a32d917bfceb6127cdc8b20669474f3
trendfollowerpl <trendfollowerpl@windowslive.com>
2018-04-24 20:18:52 +0200

    DelegateObject_Equal

--- equality/part2.cs
+++ equality/part2.cs
@@ -10,6 +10,27 @@ namespace equality
 	[TestFixture]
 	public class part2
 	{
+		private delegate void TestDelegate(string message);
 
+		[Test]
+		public void StringObject_Equal()
+		{
+			string s1 = "Ehsan Sajjad";
+			string s2 = string.Copy(s1);
+
+			Assert.That(s1.Equals((object)s2));
+		}
+
+		[Test]
+		public void DelegateObject_Equal()
+		{
+			TestDelegate delegate1 = (string s) => { };
+			TestDelegate delegate2 = (TestDelegate)delegate1.Clone();
+			TestDelegate delegate3 = delegate1;
+
+			Assert.That(delegate1.Equals((object)(delegate2)));
+			Assert.That(delegate1.Equals((object)(delegate3)));
+			Assert.That(delegate2.Equals((object)(delegate3)));
+		}
 	}
 }
